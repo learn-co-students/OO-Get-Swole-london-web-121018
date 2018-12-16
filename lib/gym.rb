a4f1f6fcc999115e75b55f9ecce270ce406894b2
github username <github email address>
2018-12-16 13:14:39 +0000

    learn submit

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,34 @@
 class Gym
-  attr_reader :name
+  attr_reader :name, :cost
+  @@all = []
 
-  def initialize(name)
+  def initialize(name,cost)
     @name = name
+    @cost = cost
+    @@all << self
+  end
+
+  def lifters
+    Lifter.all.select do |lifter|
+      lifter.gyms.include?(self)
+    end
+  end
+
+  def memberships
+    Membership.all.select do |membership|
+      membership.gym == self
+    end
+  end
+
+  def lift_total
+    total = 0
+    lifters.each do |lifter|
+        total += lifter.lift_total
+      end
+      total 
+    end
+
+  def self.all
+    @@all
   end
 end
