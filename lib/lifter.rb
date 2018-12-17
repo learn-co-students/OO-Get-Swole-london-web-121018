92416c3dad3b6c45bceaee7fc8eac49b5a4e555e
georgejeffery <georgejeffery@gmail.com>
2018-12-17 17:12:52 +0000

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,65 @@
 class Lifter
-  attr_reader :name, :lift_total
+  attr_reader :name, :lift_total, :all, :memberships, :gym
 
-  def initialize(name, lift_total)
+  @@all = []
+  @memberships = []
+  
+
+  def initialize(name, lift_total, memberships)
     @name = name
     @lift_total = lift_total
+    @memberships = []
+    @memberships << memberships
+    @gym = []
+    gym
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.average_lift
+    totallift = 0
+    self.all.each do |lifter|
+      totallift += lifter.lift_total
+    end
+    totallift/self.all.count
+  end
+
+  def gym
+    memberships.each do|membership|
+      while @gym.include?(membership.gym) == false 
+       @gym << membership.gym
+      end
+    end
+    @gym
+  end
+
+  
+
+  def join_gym(gym, cost)
+    allmemberships = Membership.all
+    membershiplevel = allmemberships.select{|membership| membership.cost == cost}
+    @memberships << membershiplevel
+    @memberships.flatten!
+    @gym << gym
+    self
+  end
+  
+  def memberships
+    @memberships.flatten
+  end
+
+  def total_cost
+    totalcost = 0
+    memberships.each do |membership|
+      totalcost += membership.cost
+    end
+    totalcost
+  end
+
+
 end
+
+
--- tools/console.rb
+++ tools/console.rb
@@ -2,7 +2,19 @@
 # We've done it for you here.
 require_relative '../config/environment.rb'
 
-# test code goes here
+cheapgym= Gym.new("cheapgym")
+expensivegym = Gym.new("expensivegym")
+fancygym = Gym.new("fancygym") 
+
+cheap = Membership.new(10, cheapgym)
+fancy = Membership.new(15, fancygym)
+expensive = Membership.new(25, expensivegym)
+
+george  = Lifter.new("George", 150, cheap)
+harry = Lifter.new("harry",300, cheap)
+sam = Lifter.new("sam", 100, fancy)
+
+
 
 binding.pry
 
