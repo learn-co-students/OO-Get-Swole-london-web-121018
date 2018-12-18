f11db03a35b24aeeb75b7ad130bbf588dcb20ca5
amaan19 <amaan.ahmed3@gmail.com>
2018-12-18 14:54:31 +0000

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -3,5 +3,31 @@ class Gym
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select {|membership| membership.gym == self}
+  end
+
+  def lifters
+    self.memberships.select{|membership| membership.lifter}
+  end
+
+  def lifter_names
+    self.lifters.select {|lifter| lifter.name}
+  end
+
+  def total_lift_total
+    combined_lift = 0
+    self.lifters.each {|lifter| combined_lift += lifter.lift_total}
+    combined_lift
+  end
+
 end
