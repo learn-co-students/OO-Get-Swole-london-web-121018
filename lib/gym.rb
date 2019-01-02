19602e4ed99470f7d1db74ee3b46af76970c7503
julesow <souleymanesow@gmail.com>
2019-01-02 10:37:04 +0000

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,32 @@
 class Gym
+  @@all = []
   attr_reader :name
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select{|membership| membership.gym == self}
+
+  end
+
+  def lifters
+    memberships.map{|membership| membership.lifter}
+  end
+  def total_lift
+    # Get the combined lift total of every lifter
+    # has a membership to that gym
+    # sum = 0
+    # lifters.each{|lift_total| sum += lift_total}
+    # sum.to_f
+    total = lifters.map{|lift| lift.lift_total }
+    # total.inject(0){|sum,x| sum + x}
+    total.inject(:+)
+
   end
 end
--- lib/membership.rb
+++ lib/membership.rb
@@ -1,7 +1,15 @@
 class Membership
-  attr_reader :cost
+  @@all =[]
+  attr_reader :cost,:gym,:lifter
 
-  def initialize(cost)
+  def initialize(cost,gym,lifter)
     @cost = cost
+    @gym = gym
+    @lifter =lifter
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 end
